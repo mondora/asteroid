1f3ecf3ed9d9b13437f7bf2ebf88d6ca8b3400eb
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-04-22 17:23:11 +0200

    Added db.find method. Version 0.1.10.

--- src/dumbdb.js
+++ src/dumbdb.js
@@ -14,6 +14,27 @@ DumbDb.prototype.get = function (id) {
 	return clone(this.itemsHash[id]);
 };
 
+DumbDb.prototype.find = function (selector) {
+	var getItemVal = function (item, key) {
+		return key.split(".").reduce(function (prev, curr) {
+			prev = prev[curr];
+			return prev;
+		}, item);
+	};
+	var keys = Object.keys(selector);
+	var matches = [];
+	this.itemsArray.forEach(function (item) {
+		for (var i=0; i<matchers.length; i++) {
+			var itemVal = getItemVal(item, keys[i]);
+			if (itemVal !== selector[keys[i]]) {
+				return;
+			}
+		}
+		matches.push(item);
+	});
+	return matches;
+};
+
 DumbDb.prototype.del = function (id) {
 	if (this.itemsHash[id]) {
 		var index = this.itemsArray.indexOf(this.itemsHash[id]);
