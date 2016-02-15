22bae63298824aa4a5c935e2c9599313251988e5
Paolo Scanferla <paolo.scanferla@mondora.com>
2016-02-15 16:00:01 +0100

    Various changes:
    - added `connected` and `disconnected` methods to ddp mixin
    - passed `autoConnect` and `autoReconnect` options to DDP constructor

--- src/base-mixins/ddp.js
+++ src/base-mixins/ddp.js
@@ -4,18 +4,38 @@
 *     it in the `ddp` property of the Asteroid instance
 *   - listens for the `connected` and `disconnected` events of the DDP instance
 *     and proxies them to the Asteroid instance
-*    - exposes the `endpoint` public property
+*   - exposes the `endpoint` public property
+*   - defines the `connect` and `disconnect` methods, used to control the ddp
+*     connection with the server
 */
 
 import DDP from "ddp.js";
 
 /*
+*   Public methods
+*/
+
+export function connect () {
+    this.ddp.connect();
+}
+
+export function disconnect () {
+    this.ddp.disconnect();
+}
+
+/*
 *   Init method
 */
 
-export function init ({endpoint, SocketConstructor = WebSocket}) {
+export function init (options) {
+    const {
+        endpoint,
+        SocketConstructor = WebSocket,
+        autoConnect,
+        autoReconnect
+    } = options;
     this.endpoint = endpoint;
-    this.ddp = new DDP({endpoint, SocketConstructor})
+    this.ddp = new DDP({endpoint, SocketConstructor, autoConnect, autoReconnect})
         .on("connected", () => this.emit("connected"))
         .on("disconnected", () => this.emit("disconnected"));
 }
